1c2dfda1a718a9f43c7fcf24707569c7300bdff7
summer-mothwood <summer.mothwood@innovation.ca.gov>
2025-05-28 15:42:58 -0700

    lint

--- transform/models/marts/daily_report/ethelo_comments.sql
+++ transform/models/marts/daily_report/ethelo_comments.sql
@@ -1,8 +1,9 @@
 --create a comments mart
 with comments as (
-select * from {{ ref('stg_comments') }}
+    select * from {{ ref('stg_comments') }}
 )
-select 
-count(distinct comment_id) as num_comments,
-count(distinct posted_by_id) as num_participants_w_commments
+
+select
+    count(distinct comment_id) as num_comments,
+    count(distinct posted_by_id) as num_participants_w_commments
 from comments
--- transform/models/marts/daily_report/ethelo_partipants.sql
+++ transform/models/marts/daily_report/ethelo_partipants.sql
@@ -1,10 +1,10 @@
 --create a participants mart
 with participants as (
-select * from {{ ref('participants') }}
+    select * from {{ ref('participants') }}
 
 )
 
-select 
-count(distinct participant_id) as num_invited,
-count(distinct (case when status = 'Joined' then participant_id end)) as num_joined
+select
+    count(distinct participant_id) as num_invited,
+    count(distinct case when status = 'Joined' then participant_id end) as num_joined
 from participants
