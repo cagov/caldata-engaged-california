8e48eb9f2b6e0d4d85611977a6afd49c245fcdfe
summer-mothwood <summer.mothwood@innovation.ca.gov>
2025-06-18 13:59:20 -0700

    adding new segments to daily_report mart

--- transform/models/marts/daily_report/mailchimp_segments.sql
+++ transform/models/marts/daily_report/mailchimp_segments.sql
@@ -1,11 +1,12 @@
 select
-    sum(case when segments like '%eaton%' then total_subscribers else 0 end) as eaton_total,
-    sum(case when segments like '%palisades%' then total_subscribers else 0 end) as palisades_total,
+    --Phase 1 LA Wildfire Segments:
+    sum(case when segments like '%eatonphase1%' then total_subscribers else 0 end) as eaton_phase1_total,
+    sum(case when segments like '%palisadesphase1%' then total_subscribers else 0 end) as palisades_phase1_total,
     sum(case when (
-        segments like '%eaton%'
-        and segments like '%palisades%'
+        segments like '%eatonphase1%'
+        and segments like '%palisadesphase1%'
     ) then total_subscribers
-    else 0 end) as eaton_and_palisades_total,
+    else 0 end) as eaton_and_palisades_phase1_total,
     sum(case when segments like '%future%' then total_subscribers else 0 end) as future_topics,
     sum(case when (
         segments like '%future%'
@@ -13,7 +14,14 @@ select
         and segments not like '%eaton%'
     ) then total_subscribers
     else 0 end) as future_topics_only,
-    sum(case when segments like '%no-interest%' then total_subscribers else 0 end) as no_interest_total,
+
+    --Phase 2 LA Wildfire Segments:
+    sum(case when segments like '%eatonphase2%' then total_subscribers else 0 end) as eaton_phase2_total,
+    sum(case when segments like '%palisadesphase2%' then total_subscribers else 0 end) as palisades_phase2_total,
+    sum(case when segments like '%nofirephase2%' then total_subscribers else 0 end) as no_fire_phase2_total,
+
+    -- Subscribed, but no segment:
+    sum(case when segments like '%nointerest%' then total_subscribers else 0 end) as no_segment_total,
     max(max_fivetran_sync_date) as max_fivetran_sync_date
 
 from {{ ref('mailchimp_subscribers_by_segment') }}
