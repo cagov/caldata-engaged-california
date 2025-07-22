6256a2386b648d1f6c098c4a43264434c505e7be
summer-mothwood <summer.mothwood@innovation.ca.gov>
2025-07-22 12:25:50 -0700

    beginning modeling of new airtable data

---
+++ transform/models/staging/ethelo_la_deliberation/stg_ethelo_la_deliberation_comments.sql
@@ -0,0 +1,18 @@
+SELECT
+    a.comment_id,
+    a.topic,
+    a.type,
+    a.target,
+    a.posted_by_id,
+    a.privacy,
+    a.content,
+    a.reply_to_id,
+    a.posted_on,
+    a.reply_count,
+    a.flag_count,
+    a.like_count,
+    a._fivetran_synced
+
+FROM {{ source('ETHELO', 'COMMENTS') }} AS a
+--  filter out comments from testers.
+INNER JOIN {{ ref('stg_participants') }} AS b ON a.posted_by_id = b.participant_id
