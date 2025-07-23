ef557d3cbbca55eedc1c6df63545928439e0ceec
summer-mothwood <summer.mothwood@innovation.ca.gov>
2025-07-23 15:10:12 -0700

    add another regular run to the cron and two new metrics for the report

--- transform/models/marts/daily_report/ethelo_participants.sql
+++ transform/models/marts/daily_report/ethelo_participants.sql
@@ -7,5 +7,8 @@ with participants as (
 select
     count(distinct participant_id) as num_invited,
     count(distinct case when status = 'Joined' then participant_id end) as num_joined,
+    count(distinct case when last_invite_sent < current_date then participant_id end) as num_invited_before_today,
+    count(distinct case when status = 'Joined' 
+        and last_invite_sent < current_date then participant_id end) as num_invited_before_today_and_joined,
     max(_fivetran_synced) as max_fivetran_sync
 from participants
