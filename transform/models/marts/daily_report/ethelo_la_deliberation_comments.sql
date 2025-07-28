86365d5c74e92390165550ff84bf369fae82a8f3
summer-mothwood <summer.mothwood@innovation.ca.gov>
2025-07-28 11:22:40 -0700

    splitting comments metrics out by evac zone

--- transform/models/marts/daily_report/ethelo_la_deliberation_comments.sql
+++ transform/models/marts/daily_report/ethelo_la_deliberation_comments.sql
@@ -1,10 +1,17 @@
 --create a comments mart
 with comments as (
     select * from {{ ref('stg_ethelo_la_deliberation_comments') }}
+),
+
+survey as (
+    select * from {{ ref('int_participant_survey_responses_wide') }}
 )
 
 select
-    count(distinct comment_id) as num_comments,
-    count(distinct posted_by_id) as num_participants_w_commments,
-    max(_fivetran_synced) as max_fivetran_sync
+    count(distinct comments.comment_id) as num_comments,
+    count(distinct comments.posted_by_id) as num_participants_w_commments,
+    survey.evacuation_zone,
+    max(comments._fivetran_synced) as max_fivetran_sync
 from comments
+left join survey on comments.posted_by_id = survey.participant_id
+group by survey.evacuation_zone
