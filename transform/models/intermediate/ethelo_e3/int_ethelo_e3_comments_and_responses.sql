c68b40cf14878d6604a779369cad2e623d12426f
summer-mothwood <summer.mothwood@innovation.ca.gov>
2025-08-15 13:55:46 -0700

    adding int and mart models for e3 ethelo

---
+++ transform/models/intermediate/ethelo_e3/int_ethelo_e3_comments_and_responses.sql
@@ -0,0 +1,30 @@
+-- this table combines comments with survey responses
+with comments as (
+    select
+        comment_id,
+        reply_to_id,
+        posted_by_id as participant_id,
+        comment_content as content,
+        target as question,
+        posted_on,
+        _file_upload_date
+    from {{ ref('stg_ethelo_e3_comments') }}
+),
+
+survey as (
+    select
+        null as comment_id,
+        null as reply_to_id,
+        participant_id,
+        answer as content,
+        question,
+        response_date as posted_on,
+        _file_upload_date
+    from {{ ref('stg_ethelo_e3_survey') }}
+    where question = 'Opening question - What makes you proud about your role in public service?'
+)
+
+select *
+from comments
+union distinct
+select * from survey
