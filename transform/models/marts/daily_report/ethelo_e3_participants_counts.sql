dac8e6ad0b258cf549408cd5b395676355a0aa1e
summer-mothwood <summer.mothwood@innovation.ca.gov>
2025-08-15 14:32:35 -0700

    -

--- transform/models/marts/daily_report/ethelo_e3_participants_counts.sql
+++ transform/models/marts/daily_report/ethelo_e3_participants_counts.sql
@@ -13,5 +13,5 @@ select
     --        status = 'Joined'
     --        and last_invite_sent < current_date then participant_id
     --end) as num_invited_before_today_and_joined,
-     max(_file_upload_date) as latest_data_download
+    max(_file_upload_date) as latest_data_download
 from participants
