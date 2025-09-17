15bce3070717ea4d18dff3414e988abbebdd0691
summer-mothwood <summer.mothwood@innovation.ca.gov>
2025-09-17 14:37:13 -0700

    lint

--- transform/models/marts/daily_report/ethelo_e3_response_counts.sql
+++ transform/models/marts/daily_report/ethelo_e3_response_counts.sql
@@ -1,35 +1,36 @@
-
 --count of participants per department -- this will need refinement as we clean and fill in dept gaps
 with dept_count as (
-    select 
-        count(distinct participant_id) as num_participants, 
-        'department' as response_type, 
-        idea_dept as response_value 
-from analytics_engca_prd.ethelo_e3.e3_participant_responses
-group by all
+    select
+        count(distinct participant_id) as num_participants,
+        'department' as response_type,
+        idea_dept as response_value
+    from analytics_engca_prd.ethelo_e3.e3_participant_responses
+    group by all
 ),
 
 --count of participants per position type dropdown response
 pos_count as (
-    select 
-        count(distinct participant_id) as num_participants, 
-        'position' as response_type, 
-        pos_type as response_value  
-from analytics_engca_prd.ethelo_e3.e3_participant_responses
-group by all),
+    select
+        count(distinct participant_id) as num_participants,
+        'position' as response_type,
+        pos_type as response_value
+    from analytics_engca_prd.ethelo_e3.e3_participant_responses
+    group by all
+),
 
 
 --count of participants per tenure dropdown response
 tenure as (
-    select 
-    count(distinct participant_id) as num_participants, 
-    'tenure' as response_type, 
-    ca_tenure as response_value  
+    select
+        count(distinct participant_id) as num_participants,
+        'tenure' as response_type,
+        ca_tenure as response_value
     from analytics_engca_prd.ethelo_e3.e3_participant_responses
-group by all)
+    group by all
+)
 
 select * from dept_count
 union all
 select * from pos_count
-union all 
+union all
 select * from tenure
