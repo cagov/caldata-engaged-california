bd4213534a72317e342a4dbb6faf7b788f239977
britt-allen <thebrittallen@gmail.com>
2025-10-22 12:45:21 -0700

    precommit fix

--- transform/models/marts/ethelo_e3/e3_problems_solutions_comments.sql
+++ transform/models/marts/ethelo_e3/e3_problems_solutions_comments.sql
@@ -24,11 +24,11 @@ combined AS (
         ps.solution_text,
         c_solution.content AS comment_linked_to_solution
 
-    FROM problems_and_solutions AS ps -- noqa: ST09
-    LEFT JOIN comments AS c_problem
-        ON c_problem.comment_id = ps.problem_comment_id
+    FROM problems_and_solutions AS ps
+    LEFT JOIN comments AS c_problem -- noqa: ST09
+        ON ps.problem_comment_id = c_problem.comment_id
     LEFT JOIN comments AS c_solution
-        ON c_solution.comment_id = ps.solution_comment_id
+        ON ps.solution_comment_id = c_solution.comment_id
 )
 
 SELECT * FROM combined
