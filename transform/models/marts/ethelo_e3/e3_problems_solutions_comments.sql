b20fc9f218f0ee8796d7bdc4a6c2bccc5b1bba90
Brittany Allen <brittany.allen@innovation.ca.gov>
2025-10-22 19:36:33 +0000

    updates based on review feedback

--- transform/models/marts/ethelo_e3/e3_problems_solutions_comments.sql
+++ transform/models/marts/ethelo_e3/e3_problems_solutions_comments.sql
@@ -1,16 +1,35 @@
-SELECT
-    c.posted_on,
-    c.content,
-    ps.problem_text,
-    ps.solution_text,
-    c.question,
-    c.comment_id,
-    c.participant_id,
-    c.reply_to_id
+WITH
+problems_and_solutions AS (
+    SELECT * FROM {{ ref('int_problem_solution_links') }}
+),
 
-FROM {{ ref('e3_comments') }} AS c
-INNER JOIN {{ ref('int_problem_solution_links') }} AS ps
-    ON
-        c.comment_id = ps.problem_comment_id
-        AND c.comment_id = ps.solution_comment_id
-ORDER BY c.posted_on DESC
+comments AS (
+    SELECT * FROM {{ ref('e3_comments') }}
+),
+
+combined AS (
+
+    SELECT
+        -- problem side
+        c_problem.comment_id AS comment_id_linked_to_problem,
+        ps.problem_comment_id AS problem_text_id,
+        ps.problem_posted_on,
+        ps.problem_text,
+        c_problem.content AS comment_linked_to_problem,
+
+        -- solution side
+        c_solution.comment_id AS comment_id_linked_to_solution,
+        ps.solution_comment_id AS solution_text_id,
+        ps.solution_posted_on,
+        ps.solution_text,
+        c_solution.content AS comment_linked_to_solution
+
+    FROM problems_and_solutions AS ps -- noqa: ST09
+    LEFT JOIN comments AS c_problem
+        ON c_problem.comment_id = ps.problem_comment_id
+    LEFT JOIN comments AS c_solution
+        ON c_solution.comment_id = ps.solution_comment_id
+)
+
+SELECT * FROM combined
+ORDER BY problem_posted_on DESC
