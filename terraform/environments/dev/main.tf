2f7173019f0909680f07ebd6d93ae03cc45fd050
“ram-kishore-odi” <ram.kishore@innovation.ca.gov>
2025-11-19 12:28:22 -0800

    Applying common module changes to EngCa env.

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -81,7 +81,7 @@ provider "snowflake" {
 ############################
 
 module "elt" {
-  source = "github.com/cagov/data-infrastructure.git//terraform/snowflake/modules/elt?ref=fe3255a"
+  source = "github.com/cagov/data-infrastructure.git//terraform/snowflake/modules/elt?ref=4d42165"
   providers = {
     snowflake.accountadmin  = snowflake.accountadmin,
     snowflake.securityadmin = snowflake.securityadmin,
