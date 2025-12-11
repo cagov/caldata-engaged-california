1112100952d948d63f31b97f6be11f64275b132e
chollingsworthAC <carl.hollingsworth@innovation.ca.gov>
2025-12-10 22:32:37 -0800

    Revert "add requests package to topic_modeling.py"
    This reverts commit 5ac65277f2c9aaa587717acd5630bf13f4ed1f7a.
    
    Revert unnecessary change to int_e3_topic_modeling.py

--- transform/models/intermediate/ethelo_e3/int_e3_topic_modeling.py
+++ transform/models/intermediate/ethelo_e3/int_e3_topic_modeling.py
@@ -224,7 +224,7 @@ def model(dbt, session: Session):
 
     dbt.config(
         materialized="table",
-        packages=["hdbscan", "numpy", "pandas", "scikit-learn", "umap-learn", "requests"],
+        packages=["hdbscan", "numpy", "pandas", "scikit-learn", "umap-learn"],
     )
 
     if dbt.this.database:
