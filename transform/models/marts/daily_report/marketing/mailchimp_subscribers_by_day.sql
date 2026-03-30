042491b2215d8a1ee350a335f8a157e5a0959eeb
summer-mothwood <summer.mothwood@innovation.ca.gov>
2026-03-30 11:31:59 -0700

    edit mailchimp mart model to include subscriber counts by day, and add last_changed field to upstream staging model for future use in counting unsubs per day

---
+++ transform/models/marts/daily_report/marketing/mailchimp_subscribers_by_day.sql
@@ -0,0 +1,23 @@
+with
+list_members as (
+    select *,
+    to_date(subscribe_timestamp) as subscribe_day
+    from {{ ref('stg_mailchimp_list_members') }}
+),
+
+subscribers_by_day as (
+    select
+        list_name,
+        subscribe_day,
+        count(distinct unique_email_id) as total_subscribers,
+        max(_fivetran_synced) as max_fivetran_sync_date
+    from list_members
+    group by list_name, subscribe_day
+
+)
+
+select * from subscribers_by_day
+
+
+
+
