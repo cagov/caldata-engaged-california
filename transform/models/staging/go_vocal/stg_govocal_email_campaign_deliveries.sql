36352600273a94d5476fad407fd2d2c46fbcdc85
chollingsworthAC <carl.hollingsworth@innovation.ca.gov>
2026-05-05 16:48:24 -0700

    Add Go Vocal staging models

---
+++ transform/models/staging/go_vocal/stg_govocal_email_campaign_deliveries.sql
@@ -0,0 +1,15 @@
+with
+
+email_campaign_deliveries as (select * from {{ source('GOVOCAL', 'EMAIL_CAMPAIGN_DELIVERIES') }})
+
+select
+    id as email_campaign_delivery_id,
+    email_campaign_id,
+    user_id,
+    delivery_status,
+    sent_at::timestamp_ltz as sent_at,
+    created_at::timestamp_ltz as created_at,
+    updated_at::timestamp_ltz as updated_at,
+    _load_date,
+    _loaded_at
+from email_campaign_deliveries
