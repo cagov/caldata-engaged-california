bfbad7f9a94c3f3433d3477a531266bdf45c83c4
chollingsworthAC <carl.hollingsworth@innovation.ca.gov>
2026-05-06 10:09:54 -0700

    Update govocal_user_counts available for discussion count, and add yml

--- transform/models/marts/go_vocal/govocal_user_counts.sql
+++ transform/models/marts/go_vocal/govocal_user_counts.sql
@@ -1,3 +1,8 @@
+-- This model joins user data with AI Survey data to calculate the count of Go Vocal users by various demographic
+-- and survey response fields.
+
+{% set total_survey_fields = 11 %}
+
 with
 
 survey as (select * from {{ ref('int_govocal_ai_survey') }}),
@@ -8,6 +13,16 @@ non_admin_users as (
     select *
     from users
     where is_admin = FALSE
+),
+
+count_specific_demographic_fields as (
+    select
+        *,
+        iff(age is not NULL, 1, 0)
+        + iff(array_size(gender_array) > 0, 1, 0)
+        + iff(array_size(race_ethnicity_array) > 0, 1, 0)
+            as demographic_fields_completed_count
+    from non_admin_users
 ),
 
 handle_multiselect_arrays as (
@@ -28,7 +43,7 @@ handle_multiselect_arrays as (
             else 'multiple'
         end as race_ethnicity
 
-    from non_admin_users
+    from count_specific_demographic_fields
 ),
 
 counts as (
@@ -46,8 +61,13 @@ counts as (
         count(distinct case when s.publication_status = 'draft' then s.author_id end) as drafts_count,
         count(distinct case when s.availability_for_discussion in ('yes', 'maybe') then s.author_id end)
             as available_for_discussion_count,
-        -- all_fields_completed_count doesn't include the user based demographic fields
-        count(distinct case when s.fields_completed_count = 8 then s.author_id end) as all_fields_completed_count
+        count(
+            distinct case
+                when
+                    s.fields_completed_count + u.demographic_fields_completed_count = {{ total_survey_fields }}
+                    then s.author_id
+            end
+        ) as all_fields_completed_count
 
     from handle_multiselect_arrays as u
     left join survey as s on u.user_id = s.author_id
