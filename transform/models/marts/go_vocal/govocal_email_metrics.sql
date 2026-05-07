732ec4efc0a0babd8aebd0a26d7f62518fab7b79
chollingsworthAC <carl.hollingsworth@innovation.ca.gov>
2026-05-07 09:02:44 -0700

    redesign and update yml for SendGrid

--- transform/models/marts/go_vocal/govocal_email_metrics.sql
+++ transform/models/marts/go_vocal/govocal_email_metrics.sql
@@ -1,12 +1,12 @@
 with
 
-email_campaigns as (select * from {{ ref('stg_govocal_email_campaigns') }}),
+email_campaign_deliveries as (select * from {{ ref('stg_govocal_email_campaign_deliveries') }}),
 
-email_campaign_deliveries as (select * from {{ ref('stg_govocal_email_campaign_deliveries') }})
+email_campaigns as (select * from {{ ref('stg_govocal_email_campaigns') }})
 
 select
-    c.email_campaign_id,
-    c.sender,
+    d.email_campaign_id,
+    iff(c.sender is null, 'sendgrid', c.sender) as sender,
     c.reply_to,
     c.subject,
     c.deliveries_count,
@@ -18,8 +18,8 @@ select
     count_if(d.delivery_status = 'clicked') as clicked,
     min(d.sent_at) as first_sent_at,
     max(d.sent_at) as last_sent_at
-from email_campaigns as c
-full outer join
-    email_campaign_deliveries as d
-    on c.email_campaign_id = d.email_campaign_id
-group by 1, 2, 3, 4, 5, 6, 7
+from email_campaign_deliveries as d
+left join
+    email_campaigns as c
+    on d.email_campaign_id = c.email_campaign_id
+group by all
