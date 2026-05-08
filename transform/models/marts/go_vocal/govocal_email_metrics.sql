029972de87d1a948c9b3a316cb906076a8f55398
chollingsworthAC <carl.hollingsworth@innovation.ca.gov>
2026-05-07 18:57:43 -0700

    Update calcs and add rate metrics. Update yml for email delivery statuses.

--- transform/models/marts/go_vocal/govocal_email_metrics.sql
+++ transform/models/marts/go_vocal/govocal_email_metrics.sql
@@ -13,9 +13,11 @@ select
     c.created_at,
     c.updated_at,
     count_if(d.delivery_status = 'clicked') as clicked,
-    clicked + count_if(d.delivery_status = 'opened') as opened,
-    opened + count_if(d.delivery_status = 'delivered') as delivered,
-    delivered + count_if(d.delivery_status = 'sent') as sent,
+    count_if(d.delivery_status in ('clicked', 'opened')) as opened,
+    count_if(d.delivery_status in ('clicked', 'opened', 'delivered', 'accepted')) as delivered,
+    count(*) as attempted,
+    case when delivered = 0 then null else clicked / delivered end as click_rate,
+    case when delivered = 0 then null else opened / delivered end as open_rate,
     min(d.sent_at) as first_sent_at,
     max(d.sent_at) as last_sent_at
 from email_campaign_deliveries as d
