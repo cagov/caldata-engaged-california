0c77f6745144bae11f570cf2b1950a72f9d1709a
summer-mothwood <summer.mothwood@innovation.ca.gov>
2026-05-18 14:54:56 -0700

    Update transform/models/staging/social/stg_instagram__media_history.sql
    Co-authored-by: chollingsworthAC <carl.hollingsworth@innovation.ca.gov>

--- transform/models/staging/social/stg_instagram__media_history.sql
+++ transform/models/staging/social/stg_instagram__media_history.sql
@@ -9,7 +9,7 @@ WITH ig AS (
         TO_VARCHAR(carousel_album_id) AS carousel_album_id,
         is_comment_enabled,
         is_story,
-        TO_DATE(created_time) AS created_time,
+        TO_TIMESTAMP(created_time) AS created_time,
         _fivetran_synced
     FROM {{ source('INSTAGRAM_BUSINESS', 'MEDIA_HISTORY') }}
 )
