61023bb7b5ca5203130807f26cc2aaf664a8d27e
Natalia Villalobos <nataliavillalobos@MacBookAir.home.local>
2026-06-02 11:32:20 -0700

    removed manual calculation

--- transform/models/intermediate/social/int_youtube_video_daily.sql
+++ transform/models/intermediate/social/int_youtube_video_daily.sql
@@ -32,8 +32,7 @@ final as (
         s.watch_time_minutes,
         s.average_view_duration_seconds,
         s.average_view_duration_percentage,
-        v.video_duration_seconds,
-        round(s.watch_time_minutes * 60.0 / nullif(v.video_duration_seconds, 0) * 100, 1) as watch_time_normalized_pct
+        v.video_duration_seconds
     from stats as s
     inner join videos as v on s.video_id = v.id
 
