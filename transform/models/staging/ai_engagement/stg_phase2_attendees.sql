d853961b0e27de248bb6d984e58c62532925df15
summer-mothwood <summer.mothwood@innovation.ca.gov>
2026-06-08 14:48:10 -0700

    add phase 2 attendee data to staging

---
+++ transform/models/staging/ai_engagement/stg_phase2_attendees.sql
@@ -0,0 +1,17 @@
+with
+
+phase_2_attendees as (select * from {{ source('FILE_DOWNLOADS', 'PH_2_ATTENDEES') }})
+
+select
+    event_type_name as event_name,
+    invitee_email,
+    invitee_first_name,
+    invitee_last_name,
+    invitee_status,
+    marked_as_no_show,
+    start_date_time,
+    end_date_time,
+    event_created_date_time,
+    _file,
+    _fivetran_synced
+from phase_2_attendees
