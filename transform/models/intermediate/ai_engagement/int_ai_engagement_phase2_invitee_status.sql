49e63ad64e35d68cd6f577f50819c859a57a93cf
summer-mothwood <summer.mothwood@innovation.ca.gov>
2026-06-17 13:19:26 -0700

    change location of source to int folder

--- transform/models/intermediate/ai_engagement/int_ai_engagement_phase2_invitee_status.sql
+++ transform/models/intermediate/ai_engagement/int_ai_engagement_phase2_invitee_status.sql
@@ -35,39 +35,40 @@ any_acceptance as (
 
 manually_matched_participants as (
     select
-        unmatched_email,
+        invitee_email,
         staff_or_moderator,
         survey_respondent_id_match,
         email_match,
         sortition_round
-    from {{ source('AI_ENGAGEMENT', 'INT_AI_ENGAGEMENT_UNMATCHED_PARTICIPANTS') }}
+    from {{ source('ZOOM', 'UNMATCHED_PARTICIPANTS') }}
     where staff_or_moderator = FALSE -- remove all staff and moderators who have signed up for time slots
 ),
 
---now we need to reconcile attendees with GV profiles. 
+--reconcile attendees with GV profiles:
 email_match as (
     select
         aa.invitee_email,
         aa.invitee_status,
         coalesce(u.user_id, um.survey_respondent_id_match) as survey_respondent_id,
-        um.email_match
+        um.email_match,
+        um.sortition_round
     from any_acceptance as aa
     left join users as u
         on lower(trim(aa.invitee_email)) = lower(trim(u.email)) -- exact matches
     left join manually_matched_participants as um
-            on lower(trim(aa.invitee_email)) = lower(trim(um.unmatched_email)) -- manually matched emails
+        on lower(trim(aa.invitee_email)) = lower(trim(um.invitee_email)) -- manually matched emails
 ),
 
 invitee_status as (
     select
-        i.sortition_round,
+        coalesce(i.sortition_round, em.sortition_round) as sortition_round,
         coalesce(i.survey_respondent_id, em.survey_respondent_id, 'Unknown email') as survey_respondent_id,
         em.invitee_email,
         em.email_match,
         coalesce(em.invitee_status, 'No Response Found') as invitee_status
     from invitees as i
     full outer join email_match as em
-        on i.survey_respondent_id = em.user_id
+        on i.survey_respondent_id = em.survey_respondent_id
 )
 
 select * from invitee_status
