1ddfdb33992546680f234eddc07477ff03d65619
chollingsworthAC <carl.hollingsworth@innovation.ca.gov>
2026-08-17 17:10:17 -0700

    Add ai_response_label to user counts mart

--- transform/models/marts/go_vocal/govocal_user_counts.sql
+++ transform/models/marts/go_vocal/govocal_user_counts.sql
@@ -6,6 +6,26 @@ with
 
 users_x_survey as (select * from {{ ref('int_govocal_users_x_ai_survey') }}),
 
+ai_response_label as (
+    select
+        survey_respondent_id,
+        case ai_response_label
+            when 'neutral' then 'mix'
+            when 'pro' then 'pos'
+            when 'anti' then 'neg'
+            else ai_response_label
+        end as ai_response_label
+    from {{ ref('int_govocal_ai_response_label') }}
+),
+
+add_ai_label as (
+    select
+        u.*,
+        a.ai_response_label
+    from users_x_survey as u
+    left join ai_response_label as a on u.survey_respondent_id = a.survey_respondent_id
+),
+
 counts as (
     select
         role_at_work,
@@ -15,6 +35,7 @@ counts as (
         age,
         gender_category as gender,
         race_ethnicity_category as race_ethnicity,
+        ai_response_label,
         count(distinct user_id) as gv_users_count,
         count(distinct survey_respondent_id) as respondents_count,
         count(distinct case when publication_status = 'published' then survey_respondent_id end) as submitted_count,
@@ -31,7 +52,7 @@ counts as (
 
         max(_loaded_at) as data_loaded_at
 
-    from users_x_survey
+    from add_ai_label
     group by
         role_at_work,
         county,
@@ -39,7 +60,8 @@ counts as (
         field_of_work,
         age,
         gender_category,
-        race_ethnicity_category
+        race_ethnicity_category,
+        ai_response_label
 )
 
 select * from counts
